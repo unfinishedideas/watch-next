bc92bb54e3449a6935ef732e4dbd4438c5543d11
unfinishedideas <unfinishedideas@gmail.com>
2025-03-04 20:35:37 -0800

    added openapi, began scaffolding routes for app

--- backend/WatchNext/Program.cs
+++ backend/WatchNext/Program.cs
@@ -1,6 +1,39 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// For now, just leaving these here in one big file until it gets complicated enough to move :)
+// GET
+app.MapGet("/users", () => "Users: Not setup yet :)");
+app.MapGet("/users/{id}", (int id) => $"Users: Not setup yet, but you passed id: {id}");
+
+app.MapGet("/lists", () => "Lists: Not setup yet :)");
+app.MapGet("/lists/{id}", (int id) => $"Lists: Not setup yet, but you passed id: {id}");
+
+app.MapGet("/movies", () => "Movies: Not setup yet :)");
+app.MapGet("/movies/{id}", (int id) => $"Movies: Not setup yet, but you passed id: {id}");
+
+// POST
+//app.MapPost("/users", ); 
+//app.MapPost("/lists", ); 
+//app.MapPost("/movies", ); 
+
+// UPDATE
+//app.MapPut("/users", ); 
+//app.MapPut("/lists", ); 
+//app.MapPut("/movies", ); 
+
+// DELETE
+//app.MapDelete("/users/{id}",  (int id) => ); 
+//app.MapDelete("/lists/{id}",  (int id) => ); 
+//app.MapDelete("/movies/{id}", (int id) => ); 
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.Run();
---
+++ backend/WatchNext/README.md
@@ -0,0 +1,10 @@
+# Watch Next! - Backend Service
+
+## What it is
+
+This is a basic backend service for the Watch Next! application
+
+## Technology
+
+- [ASP.NET 9.0](https://dotnet.microsoft.com/en-us/download/dotnet/9.0)
+
