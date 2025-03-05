e247e68fcfd6ede3aef40f479b9f5613cb1ecda4
unfinished ideas <unfinishedideas@gmail.com>
2025-03-05 13:33:59 -0800

    adjusted moviecard css

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -13,8 +13,8 @@ const MovieCard: React.FC<MovieCardProps> = ({data, index} : MovieCardProps) =>
         <div className="movie-card-container">
             <h3 className="movie-title-text">{index+1}: {data.title}</h3>
             <img src={Logo} className="movie-poster-preview"/>
-            <p className="info-small">Year: {data.year}</p>
             <p className="info-small">Director: {data.director}</p>
+            <p className="info-small">Year: {data.year}</p>
             <p className="info-small">Rating: {data.rating}</p>
         </div>
     )
