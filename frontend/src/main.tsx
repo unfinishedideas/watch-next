75ba2bb77a0919069dca89736b090443a832426f
unfinishedideas <unfinishedideas@gmail.com>
2025-03-13 20:48:39 -0700

    removed react-query for now as I am not using it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,18 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-
-const queryClient = new QueryClient();
 
 // NOTE! StrictMode renders components twice in dev mode
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
