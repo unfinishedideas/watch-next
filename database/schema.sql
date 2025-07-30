01e08c226fc5db47ca2990978b5558c1f2de69e5
unfinishedideas <unfinishedideas@gmail.com>
2025-07-30 00:38:16 -0700

    added movie_lists, movies and join tables to postgresDB

--- database/schema.sql
+++ database/schema.sql
@@ -6,3 +6,29 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE IF NOT EXISTS movie_lists (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    list_title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_movie_lists (
+    user_id UUID NOT NULL,
+    list_id UUID NOT NULL,
+    PRIMARY KEY (user_id, list_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (list_id) REFERENCES movie_lists(id)
+);
+
+CREATE TABLE IF NOT EXISTS movies (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    movie_title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS movie_list_movies (
+    movie_id UUID NOT NULL,
+    list_id UUID NOT NULL,
+    PRIMARY KEY (movie_id, list_id),
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (list_id) REFERENCES movie_lists(id)
+);
