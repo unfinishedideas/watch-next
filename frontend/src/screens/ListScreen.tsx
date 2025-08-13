f78811aa8762a3383f39d993e6826488db4fa964
unfinishedideas <unfinishedideas@gmail.com>
2025-08-13 14:20:23 -0700

    updated css styles for list screen

--- frontend/src/screens/ListScreen.tsx
+++ frontend/src/screens/ListScreen.tsx
@@ -1,66 +1,61 @@
-import './Screen.css';
-import Movie from '../classes/movie.ts';
-import MovieCard from '../components/MovieCard.tsx';
-import { useQuery } from '@tanstack/react-query';
+import "./Screen.css";
+import Movie from "../classes/movie.ts";
+import MovieCard from "../components/MovieCard.tsx";
+import { useQuery } from "@tanstack/react-query";
 import { NavLink, useLocation } from "react-router";
-import { GetMovieListMovies } from '../api/ListApi.ts';
+import { GetMovieListMovies } from "../api/ListApi.ts";
 
+const ListScreen: React.FC = () => {
+  const location: Location<any> = useLocation();
+  const listData: List[] = location.state?.listData;
+  const users: User[] = location.state?.users;
 
-
-const ListScreen: React.FC = () =>
-{
-    const location = useLocation();
-    const listData = location.state?.listData;
-    const users = location.state?.users;
-
-    const listMovies = useQuery({
-        queryKey: [`list_movies_${listData.id}`],
-        queryFn: () => GetMovieListMovies(listData.id),
-    });
-
-
+  const listMovies = useQuery({
+    queryKey: [`list_movies_${listData.id}`],
+    queryFn: () => GetMovieListMovies(listData.id),
+  });
 
   if (listMovies.isPending) {
-    return(
-        <div className="screen-container">
-            <h2 className="screen-title">{listData.title}</h2>
-            <h3>Loading list...</h3>
-        </div>
-    )
+    return (
+      <div className="screen-container">
+        <h2 className="screen-title">{listData.title}</h2>
+        <h3>Loading list...</h3>
+      </div>
+    );
   }
   if (listMovies.error) {
-    return(
-        <div className="screen-container">
-            <h2 className="screen-title">{listData.title}</h2>
-            <h3>Unable to load list!</h3>
-        </div>
-    )
+    return (
+      <div className="screen-container">
+        <h2 className="screen-title">{listData.title}</h2>
+        <h3>Unable to load list!</h3>
+      </div>
+    );
   } else {
-    const fetchedMovies :Movie[] = listMovies.data.sort((a, b) => a.movie_order - b.movie_order);
-    return(
-        <div className="screen-container">
-            <h2 className="screen-title">{listData.title}</h2>
-            <p>Created By:</p>
-            {users.map((user, index) => (
-                <div key={index}>
-                  <NavLink to={`user/${user.username}`}>
-                    {user.username}&nbsp;&nbsp;&nbsp;
-                  </NavLink>
-                </div>
-            ))}
-            <h3 className="screen-subtitle">Movies</h3>
-            {fetchedMovies.map((movie: Movie, index: number) => 
-            {
-                const release_string = new Date(movie.release_date).getFullYear();
-                return(    
-                    <MovieCard data={movie} key={index}/>
-                )
-            }
-            )}
+    const fetchedMovies: Movie[] = listMovies.data.sort(
+      (a, b) => a.movie_order - b.movie_order
+    );
+    return (
+      <div className="screen-container">
+        <h2 className="screen-title">{listData.title}</h2>
+        <p>Created By:</p>
+        <div className="user-card-container">
+          {users.map((user: User, index: number) => (
+            <div className="list-user-container" key={index}>
+              {index % 4 === 0 && index !== 0 && <br />}
+              <NavLink to={`user/${user.username}`}>
+                {user.username}&nbsp;&nbsp;&nbsp;
+              </NavLink>
+            </div>
+          ))}
         </div>
-    )
+        <h3 className="screen-subtitle">Movies</h3>
+        {fetchedMovies.map((movie: Movie, index: number) => {
+          return <MovieCard data={movie} key={index} />;
+        })}
+      </div>
+    );
   }
-}
+};
 
 export default ListScreen;
 
