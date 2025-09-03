1f914f12fbdc49c3257c12ed446d21be19301c55
unfinishedideas <unfinishedideas@gmail.com>
2025-09-03 14:07:25 -0700

    removed old frontend files

--- frontend-old/src/api/Vars.ts
+++
@@ -1,3 +0,0 @@
-// TODO: Put this in an environment variable
-const base_url = "http://localhost:5025"
-export {base_url};
--- frontend-old/src/context/UserContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { UserContextType } from "../classes/User.ts";
-
-const UserContext = createContext<UserContextType | undefined>(undefined);
-
-export default UserContext;
